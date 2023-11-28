c58a092d6a9970b1959ff48ce22923ea5484d1ea
SouhailEtudiant <Souhaiel.Hajri@etudiant-isi.utm.tn>
2023-11-28 20:35:23 +0100

    commit

--- PublishToTopic.py
+++ PublishToTopic.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+# test
 import time
 import paho.mqtt.client as paho
 from paho import mqtt
